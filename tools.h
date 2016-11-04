fc976748cc2389ccdd7d5b7f646cbf4033a85bff
Oleksandra Baga <oleksandra.baga@gmail.com>
2016-11-04 15:51:47 +0100

    Declaration for MacOS added in Function clearScreen

--- tools.h
+++ tools.h
@@ -4,6 +4,7 @@
 
 #ifndef TOOLS_TOOLS_H
 #define TOOLS_TOOLS_H
+#include <stdlib.h>
 
 //TODO: waitForEnter
 //TODO: printLine
@@ -39,6 +40,10 @@ void clearScreen()
 #ifdef unix
     printf("\033[2J");
 #endif
+
+#ifdef __APPLE__
+    system("clear");
+#endif
 }
 
 /***************************************************************************
@@ -51,7 +56,8 @@ int askYesOrNo(char *text) {
 
     char Abfrage;
     int Erg;
-    do{
+    do
+    {
         //   Beispielausgabe("Möchten Sie noch einmal? (j/n) ", 'I');
         printf(text);
         Erg = scanf("%c", &Abfrage);
@@ -59,7 +65,7 @@ int askYesOrNo(char *text) {
 
         if(Abfrage != 'J' && Abfrage != 'j' && Abfrage != 'n' && Abfrage != 'N')
             Erg = 0;
-    }while(!Erg);
+    } while(!Erg);
     if(Abfrage == 'j' || Abfrage == 'J')
         return 1;
     else
