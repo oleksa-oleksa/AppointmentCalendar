c5c13eb6604054a3e9fcc5e264f8d3e334c760d3
Oleksandra Baga <oleksandra.baga@gmail.com>
2016-11-04 16:17:30 +0100

    Tools.h is divided to c- und h- files

---
+++ tools.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//TODO: waitForEnter
+//TODO: printLine
+
+/***************************************************************************
+ *  function:    clearBuffer
+ ***************************************************************************/
+void clearBuffer()
+{
+    char c;
+    do
+    {
+        scanf("%c", &c);
+    } while(c != '\n');
+}
+
+
+/***************************************************************************
+ *  function:    clearScreen
+ ***************************************************************************/
+void clearScreen()
+{
+#ifdef _WIN32
+    system("cls");
+#endif //WIN_32
+
+#ifdef unix
+    printf("\033[2J");
+#endif
+
+#ifdef __APPLE__
+    system("clear");
+#endif
+}
+
+/***************************************************************************
+ *  function:    askYesOrNo
+ ***************************************************************************/
+int askYesOrNo(char *text)
+{
+    char Abfrage;
+    int Erg;
+    do
+    {
+        //   Beispielausgabe("Möchten Sie noch einmal? (j/n) ", 'I');
+        printf(text);
+        Erg = scanf("%c", &Abfrage);
+        clearBuffer();
+
+        if(Abfrage != 'J' && Abfrage != 'j' && Abfrage != 'n' && Abfrage != 'N')
+            Erg = 0;
+    } while(!Erg);
+
+    if(Abfrage == 'j' || Abfrage == 'J')
+        return 1;
+    else
+        return 0;
+}
+
--- tools.h
+++ tools.h
@@ -1,73 +1,33 @@
-//
-// Created by Tim on 02.06.2016.
-//
-
 #ifndef TOOLS_TOOLS_H
-#define TOOLS_TOOLS_H
-#include <stdlib.h>
+#define TOOLS_H TOOLS_H
+
 
 //TODO: waitForEnter
 //TODO: printLine
 
-/***************************************************************************
- *  function:    clearBuffer
- *  description: clears the input buffer
- *  parameter:   -
- *  result :     returns 0
- ***************************************************************************/
-void clearBuffer(){
-    char c;
-    do{
-        scanf("%c", &c);
-    }while(c != '\n');
-}
+    /***************************************************************************
+     *  function:    clearBuffer
+     *  description: clears the input buffer
+     *  parameter:   -
+     *  result :     returns 0
+     ***************************************************************************/
+    void clearBuffer();
 
 
-/***************************************************************************
- *  function:    clearScreen
- *  description: clears the console screen
- *  parameter:   -
- *  result :     -
- ***************************************************************************/
-void clearScreen()
-{
-#ifdef _WIN32
-    system("cls");
-#endif //WIN_32
+    /***************************************************************************
+     *  function:    clearScreen
+     *  description: clears the console screen
+     *  parameter:   -
+     *  result :     -
+     ***************************************************************************/
+    void clearScreen();
 
-#ifdef unix
-    printf("\033[2J");
-#endif
-
-#ifdef __APPLE__
-    system("clear");
-#endif
-}
-
-/***************************************************************************
- *  function:    askYesOrNo
- *  description: Print out a message and take an input (j/n).
- *  parameter:   text: the message (question) to be asked.
- *  result :     returns 1 if the input is 'j'; 0 if it's 'n'.
- ***************************************************************************/
-int askYesOrNo(char *text) {
-
-    char Abfrage;
-    int Erg;
-    do
-    {
-        //   Beispielausgabe("Möchten Sie noch einmal? (j/n) ", 'I');
-        printf(text);
-        Erg = scanf("%c", &Abfrage);
-        clearBuffer();
-
-        if(Abfrage != 'J' && Abfrage != 'j' && Abfrage != 'n' && Abfrage != 'N')
-            Erg = 0;
-    } while(!Erg);
-    if(Abfrage == 'j' || Abfrage == 'J')
-        return 1;
-    else
-        return 0;
-}
+    /***************************************************************************
+     *  function:    askYesOrNo
+     *  description: Print out a message and take an input (j/n).
+     *  parameter:   text: the message (question) to be asked.
+     *  result :     returns 1 if the input is 'j'; 0 if it's 'n'.
+     ***************************************************************************/
+    int askYesOrNo(char *);
 
 #endif //TOOLS_TOOLS_H
