21794a572b98d0e5a215ea64b9a758ada8a0b78a
Oleksandra Baga <oleksandra.baga@gmail.com>
2016-11-11 11:27:44 +0100

    abgabe

--- menu.c
+++ menu.c
@@ -10,23 +10,27 @@
 int getMenu(char *Title, char* Points[], int anz){
     int valid = 0;
     int choice;
-    do{
+    do
+    {
         int i = 0;
         clearScreen();
         printf("%s \n",Title);
         printLine('=', strlen(Title));
-        for (0; i < anz; ++i) {
+        printf("\n");
+        for (0; i < anz; ++i)
+        {
             printf("%i. %s \n",i+1,Points[i]);
         }
         printf("\nBitte wählen Sie einen Menüpunkt: ");
         valid = scanf("%i",&choice);
         clearBuffer();
-        if(choice > anz || choice <= 0){
+        if(choice > anz || choice <= 0)
+        {
             printf("Fehler, keine gültige Menüwahl \n\n");
             valid = 0;
         }
 
-    }while(!valid);
+    } while(!valid);
 
     return choice;
 
